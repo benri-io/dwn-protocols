1a14777055c2dd4f031831d36d2734b395d272f7
Andor Kesselman <andor@henosisknot.com>
2023-04-11 14:11:43 +0530

    added switch

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -75,7 +75,7 @@ const config = {
     ({
       colorMode: {
         defaultMode: "dark",
-        disableSwitch: true,
+        disableSwitch: false,
         respectPrefersColorScheme: false,
       },
       // Replace with your project's social card
